3f49b1d53d4965894f3063333d088e9f05d23bf4
angela <angelabecerraforero@gmail.com>
2023-01-25 10:37:30 -0500

    Components for Dashboard development are created

--- prueba-mensajeros-urbanos/src/app/app.module.ts
+++ prueba-mensajeros-urbanos/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TopWidgetsComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
